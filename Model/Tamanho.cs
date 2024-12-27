5322e064e61e8fdfb18d78bb3555675a2bae8b3c
Eduardo Rodolfo de Simas <eduardorodolfosimas@gmail.com>
2024-12-27 10:39:25 -0300

    feat: tamanho model

---
+++ Model/Tamanho.cs
@@ -0,0 +1,9 @@
+namespace ControleDeEstoque.Model;
+
+public class Tamanho
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public DateTime DataInsercao { get; set; } = DateTime.Now; 
+}
